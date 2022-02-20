8805c34ad47fdd20b7588439a6e6fcb8832f4a33
FERM4T <ferperezabad@gmail.com>
2022-02-20 12:52:08 +0100

    Codigo algo mas legible + Pruebas + metodo equals(revisar)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,10 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        //TableWithLabels t = CSV.readTableWithLabels("iris.csv");
 
-        TableWithLabels t = CSV.readTableWithLabels("Files/tableLabels.csv");
-        Table tNL = CSV.readTable("Files/tableNoLabel.csv");
-        Table tNL2 = CSV.readTable("Files/tableNoLabel.csv");
+        TableWithLabels t = CSV.readTableWithLabels("Files/iris.csv");
+        Table tNL = CSV.readTable("Files/miles_dollars.csv");
 
-        tNL.equals(tNL2);
         System.out.println("asdf");
     }
 }
