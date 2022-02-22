a23e2c63be8aaa585fbc367b1e65f7c4e1e98540
FERM4T <ferperezabad@gmail.com>
2022-02-22 13:31:47 +0100

    Sesión 2

---
+++ src/main/java/LinealRegression/LinealRegression.java
@@ -0,0 +1,91 @@
+package LinealRegression;
+
+import Table.*;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class LinealRegression {
+
+    private Double alfa;
+    private Double beta;
+
+    public LinealRegression(){
+
+        alfa = 0.0;
+        beta = 0.0;
+    }
+
+    private Double mean(List<Double> array){
+        Double sum = 0.0;
+
+        for (Double element : array){
+            sum += element;
+        }
+
+        return sum/array.size();
+
+    }
+
+    private Double createAlfa(Double meanX, Double meanY, List<Double> listX, List<Double> listY){
+
+        Iterator<Double> iterX = listX.listIterator();
+        Iterator<Double> iterY = listY.listIterator();
+
+        double numerator = 0.0;
+        double denominator = 0.0;
+
+        while(iterX.hasNext()){
+            double X = iterX.next();
+            double Y = iterY.next();
+
+            numerator += (X - meanX) * (Y - meanY);
+            denominator += Math.pow(X - meanX, 2);
+
+        }
+
+        return numerator/denominator;
+    }
+
+    private Double createBeta(Double meanX, Double meanY, Double alfa){
+
+        return meanY - alfa * meanX;
+    }
+
+
+    public void train(Table table){
+
+
+        List<Double> colX = table.getColumnAt(0);
+        List<Double> colY = table.getColumnAt(1);
+
+        Double meanX = mean(colX);
+        Double meanY = mean(colY);
+
+        alfa = createAlfa(meanX, meanY, colX, colY);
+        beta = createBeta(meanX, meanY, alfa);
+
+
+
+    }
+
+    public Double estimate(Double sample){
+        return alfa*sample + beta;
+    }
+
+    public Double getAlfa() {
+        return alfa;
+    }
+
+    public Double getBeta() {
+        return beta;
+    }
+
+    public void setAlfa(Double a){
+        alfa = a;
+    }
+
+    public void setBeta(Double b){
+        beta = b;
+    }
+}
---
+++ src/test/java/LinealRegression/LinealRegressionTest.java
@@ -0,0 +1,56 @@
+package LinealRegression;
+
+import CSV.CSV;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import Table.*;
+
+class LinealRegressionTest {
+
+    private static LinealRegression lr;
+
+    private static Table testTable;
+
+    @BeforeAll
+    static void initAll() {
+
+        try{
+            lr = new LinealRegression();
+            testTable = CSV.readTable("Files/miles_dollars.csv");
+
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    @DisplayName("Train test")
+    @Test
+    void train() {
+
+        lr.train(testTable);
+
+        assertEquals(1.255, lr.getAlfa(), 0.001);
+        assertEquals(274.85, lr.getBeta(), 0.001);
+    }
+
+    @DisplayName("Estimate test")
+    @Test
+    void estimate() {
+
+        lr.setAlfa(1.255);
+        lr.setBeta(274.85);
+
+        assertEquals(277.36,lr.estimate(2.0),0.1);
+
+
+
+    }
+}
