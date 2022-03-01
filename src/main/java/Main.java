4749058a9757c3ba1ad2d9fe3e19f0c27703b98d
al404315 <Node1234#>
2022-03-01 14:02:04 +0100

    Sesion 03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,13 @@
 import CSV.CSV;
 import LinealRegression.LinealRegression;
 import Table.*;
+import KNN.*;
+import java.util.List;
 
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-
-        TableWithLabels t = CSV.readTableWithLabels("Files/iris.csv");
-        Table tNL = CSV.readTable("Files/miles_dollars.csv");
-        LinealRegression lr = new LinealRegression();
-        lr.train(tNL);
 
         System.out.println("asdf");
     }
